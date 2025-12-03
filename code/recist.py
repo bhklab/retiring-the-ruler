a05cc7063fda1fdeec21a9bd71b59a27b766d937
Katy Scott <k.l.scott16@gmail.com>
2025-12-03 15:40:47 -0500

    feat: function for selecting target lesions and calculating accuracy of response by lesion count

--- code/recist.py
+++ code/recist.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 
 
-def recist_assess(lesion_data:pd.DataFrame) -> list:
-
+def recist_assess(lesion_data:pd.DataFrame) -> pd.DataFrame:
+    """Calculate RECIST response category from lesion diameter data."""
     patients, lesion_counts = np.unique(lesion_data['patient_id'], return_counts=True)
 
     # Calculate the sum of longest diameters (SLD) for pre, post
@@ -27,11 +27,73 @@ def recist_assess(lesion_data:pd.DataFrame) -> list:
         elif SLD_chg[idx] == -100:
             RECIST_response.append('CR')
 
-    synth_response = pd.DataFrame({'patient_id': patients, 
+    patient_response = pd.DataFrame({'patient_id': patients, 
                                 'num_lesions': lesion_counts, 
                                 'SLD_pre': SLD_pre, 
                                 'SLD_post': SLD_post, 
                                 'SLD_chg': SLD_chg, 
                                 'RECIST (all)': RECIST_response})
     
-    return synth_response
+    return patient_response
+
+
+
+def select_target_lesions(num_lesions:int,
+                          lesion_data:pd.DataFrame,
+                          random_seed: int | None = None) -> pd.DataFrame:
+    """Randomly select specified number of target lesions from lesion data, ensuring no more than 2 are selected for each location
+       following RECIST specifications.
+       
+       Returns the selected target lesion rows from lesion data.
+       """
+    # Initialize random number generator
+    rng = np.random.default_rng(random_seed)
+
+    patients, lesion_counts = np.unique(lesion_data['patient_id'], return_counts=True)
+
+    # ensure that there is not more than 2 target lesions per location
+    target_lesions = []
+    for patient in patients:
+        # get the locations for the patient
+        locations = np.unique(lesion_data['location'][lesion_data['patient_id'] == patient])
+        selected_lesion_idx = []
+
+        # for each location, select up to 2 lesions
+        for location in locations:
+            # get the indices of the lesions at the location
+            targets_at_loc_idx = lesion_data[(lesion_data['patient_id'] == patient) & (lesion_data['location'] == location)].index
+            
+            # select up to 2 lesions
+            if len(targets_at_loc_idx) > 2:
+                # select 2 random indices
+                targets_at_loc_idx = rng.choice(list(targets_at_loc_idx), 2, replace=False)
+                selected_lesion_idx.extend(targets_at_loc_idx)
+            else:
+                selected_lesion_idx.extend(targets_at_loc_idx)
+        
+        # If there are more selected lesions from this location than the required number of lesions, randomly select from these
+        if len(selected_lesion_idx) > num_lesions:
+            target_lesions.extend(rng.choice(selected_lesion_idx, num_lesions))
+        else:
+            target_lesions.extend(selected_lesion_idx)
+    
+    # Select out the rows of the lesion data for the selected target lesions
+    selected_target_lesions = lesion_data.copy().iloc[target_lesions]
+    # Sort the selected lesion data by index and return
+    return selected_target_lesions.sort_index()
+
+
+
+
+def recist_accuracy_by_target_count(patient_response: pd.DataFrame,
+                                    max_targets: int = 11):
+
+    accuracy = []
+    for num_targets in range(1, max_targets):
+        # Get patients with more than num_targets lesions
+        pat_idxs = patient_response['num_lesions'] > num_targets
+
+        # Get percentage of patients that have the same response category for RECIST (all) as RECIST (num_targets targets) 
+        accuracy.append(np.sum(patient_response['RECIST (all)'][pat_idxs] == patient_response[f"RECIST ({num_targets} targets)"][pat_idxs]) / np.sum(pat_idxs) * 100)
+
+    return accuracy
